72c1555f2a74e9e542a483cc8ee57892827e0617
b3n <its.benkm@gmail.com>
2026-01-06 10:09:03 +0300

    fix-build-failure

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -25,7 +25,7 @@ export default async function Page(props: {
         </h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search />
+        <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
